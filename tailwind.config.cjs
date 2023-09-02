b30a29f06d701f62838c7ef5e9c5d764ca711031
Luis Gomez <luisfgomezortiz@gmail.com>
2023-09-03 00:35:03 +0200

    Colors defined for funky style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				primary: '#D589F3',
+				'primary-alt': '#CF63FC',
+				secondary: '#5964D1',
+				highlight:'#D7F440',
+				black:'#000000',
+				'black-alt':'#1A191D',
+			}
+		},
 	},
 	plugins: [],
 }
