a75fe342716970c2d255550f16d6932b52f36ed3
Luis Gomez <luisfgomezortiz@gmail.com>
2023-09-04 12:39:44 +0200

    Body font defined, changes con navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,10 @@ module.exports = {
 				highlight:'#D7F440',
 				black:'#000000',
 				'black-alt':'#1A191D',
-			}
+			},
+      fontFamily:{
+        primary:['Varela', 'sans-serif']
+      }
 		},
 	},
 	plugins: [],
