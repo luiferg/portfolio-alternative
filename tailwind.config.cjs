4e78712d56132a765e24680aa33131725d18c536
Luis Gomez <luisfgomezortiz@gmail.com>
2023-09-04 22:22:00 +0200

    changes on Hero section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors:{
-				primary: '#D589F3',
-				'primary-alt': '#CF63FC',
-				secondary: '#5964D1',
-				highlight:'#D7F440',
-				black:'#000000',
-				'black-alt':'#1A191D',
-			},
-      fontFamily:{
-        primary:['Varela', 'sans-serif']
-      }
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#D589F3',
+        'primary-alt': '#CF63FC',
+        secondary: '#5964D1',
+        highlight: '#D7F440',
+        black: '#000000',
+        'black-alt': '#1A191D',
+      },
+      fontFamily: {
+        primary: ['Acumin', 'sans-serif'],
+        secondary: ['Dolego', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 }
